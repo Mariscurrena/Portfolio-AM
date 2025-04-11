1669711f12e7d5f0011949b2b48b336df051d879
Steve <mariscurrenalae@gmail.com>
2025-04-10 18:03:25 -0600

    Adding Experience section (Only Options)

--- js/script.js
+++ js/script.js
@@ -1,3 +1,25 @@
 function projecturl(url) {
     window.open(url, '_blank');
 }
+
+//---------------Modal Windows---------------//
+//Modal Window Open
+var botones = document.querySelectorAll('.openModal');
+
+botones.forEach(function(boton) {
+    boton.onclick = function() {
+        var modalId = boton.getAttribute('data-modal');
+        var modal = document.getElementById(modalId);
+        modal.classList.add('show');
+    }
+});
+
+//Close Window
+window.onclick = function(event) {
+    var modales = document.querySelectorAll('.modal');
+    modales.forEach(function(modal) {
+        if (event.target == modal) {
+            modal.classList.remove('show');
+        }
+    });
+}
